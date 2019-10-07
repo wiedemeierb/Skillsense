d9fd3b2fa341310e7c6fa4d1517485900ce8f153
dmheisel <d.m.heisel@gmail.com>
2019-10-07 13:06:34 -0500

    mentor router updated with route to /api/mentors/pending to get mentors pending admin approval

--- server/routes/mentor.router.js
+++ server/routes/mentor.router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/all', (req, res) => {
     const queryText = `SELECT * FROM "student_mentor";`
     pool.query(queryText)
         .then((result) => {
@@ -11,6 +11,29 @@ router.get('/', (req, res) => {
             console.log(error);
             res.sendStatus(500);
         })
+});
+
+router.get('/pending', (req, res) => {
+    //query to get all mentors in system with pending status
+    const queryText = `
+    SELECT * from users
+	JOIN user_type
+    ON user_type.id = users.access_id
+    JOIN mentor_status
+    ON users.approved_mentor = mentor_status.id
+	WHERE user_type.access ILIKE 'Mentor' AND mentor_status.status ILIKE 'Pending Approval';`
+
+    pool.query(queryText)
+        .then(result => {
+            console.log('successful GET of mentors pending approval');
+            res.send(result.rows)
+        })
+        .catch(error => {
+            console.log('error on GET of mentors pending approval: ', error);
+            res.sendStatus(500)
+        })
+
+    ;
 })
 
 module.exports = router;
