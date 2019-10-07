fa4e47978dca020d4dce5e877b5a5d6cbec80836
dmheisel <d.m.heisel@gmail.com>
2019-10-07 14:25:19 -0500

    changed routes in app.js to protected routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -48,76 +48,52 @@ class App extends Component {
 
   render() {
     return (
-      <MuiThemeProvider theme={theme}>
-        <CssBaseline />
-        <Router>
-          <div>
-            <Nav />
-            <Switch>
-              {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
-              <Redirect exact from="/" to="/home" />
-              {/* Visiting localhost:3000/about will show the about page.
+			<MuiThemeProvider theme={theme}>
+				<CssBaseline />
+				<Router>
+					<div>
+						<Nav />
+						<Switch>
+							{/* Visiting localhost:3000 will redirect to localhost:3000/home */}
+							<Redirect exact from='/' to='/home' />
+							{/* Visiting localhost:3000/about will show the about page.
               This is a route anyone can see, no login necessary */}
-              <Route exact path="/about" component={AboutPage} />
-              {/* For protected routes, the view could show one of several things on the same route.
+							<Route exact path='/about' component={AboutPage} />
+							{/* For protected routes, the view could show one of several things on the same route.
               Visiting localhost:3000/home will show the UserPage if the user is logged in.
               If the user is not logged in, the ProtectedRoute will show the 'Login' or 'Register' page.
               Even though it seems like they are different pages, the user is always on localhost:3000/home */}
-              <Route
-                exact
-                path="/home" //user profile page
-                component={UserPage}
-              />
-              {/* This works the same as the other protected route, except that if the user is logged in,
+							<ProtectedRoute
+								exact
+								path='/home' //user profile page
+								component={UserPage}
+							/>
+							{/* This works the same as the other protected route, except that if the user is logged in,
               they will see the info page instead. */}
-              <Route
-                exact
-                path="/info"
-                component={InfoPage}
-              />
-              <Route
-                exact
-                path="/search/jobs"
-                component={JobSearch}
-              />
-              <Route
-                exact
-                path="/search/mentors"
-                component={MentorSearch}
-              />
-              <Route
-                exact
-                path="/jobs"
-                component={MyJobs}
-              />
-              <Route
-                exact
-                path="/mentors"
-                component={MyMentorships}
-              />
-              <Route
-                exact
-                path="/jobs/detail"
-                component={JobDetail}
-              />
-              <Route
-                exact
-                path="/jobs/detail/apply"
-                component={JobApplication}
-              />
-              <Route
-                exact
-                path="/admin"
-                component={MentorReview}
-              />
-              {/* If none of the other routes matched, we will show a 404. */}
-              <Route render={() => <h1>404</h1>} />
-            </Switch>
-            <Footer />
-          </div>
-        </Router>
-      </MuiThemeProvider>
-    );
+							<ProtectedRoute exact path='/info' component={InfoPage} />
+							<ProtectedRoute exact path='/search/jobs' component={JobSearch} />
+							<ProtectedRoute
+								exact
+								path='/search/mentors'
+								component={MentorSearch}
+							/>
+							<ProtectedRoute exact path='/jobs' component={MyJobs} />
+							<ProtectedRoute exact path='/mentors' component={MyMentorships} />
+							<ProtectedRoute exact path='/jobs/detail' component={JobDetail} />
+							<ProtectedRoute
+								exact
+								path='/jobs/detail/apply'
+								component={JobApplication}
+							/>
+							<ProtectedRoute exact path='/admin' component={MentorReview} />
+							{/* If none of the other routes matched, we will show a 404. */}
+							<Route render={() => <h1>404</h1>} />
+						</Switch>
+						<Footer />
+					</div>
+				</Router>
+			</MuiThemeProvider>
+		);
   }
 }
 
