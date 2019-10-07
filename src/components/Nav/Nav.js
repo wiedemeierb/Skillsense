1d8cc442d4b92b29f04c6cfed6eec83179501536
Allyson Holdahl <allyson.holdahl@gmail.com>
2019-10-07 13:45:34 -0500

    update nav bar styling

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,26 +3,25 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import './Nav.css';
+import skillSenseLogo from '../../skillSenseLogo.svg';
 
 const Nav = (props) => (
   <div className="nav">
     <Link to="/home">
-      <h2 className="nav-title">Prime Solo Project</h2>
+      {/* <h2 className="nav-title">Prime Solo Project</h2> */}
+      <img alt="skill-sense logo" src={skillSenseLogo}/>
     </Link>
     <div className="nav-right">
       <Link className="nav-link" to="/home">
         {/* Show this link if they are logged in or not,
         but call this link 'Home' if they are logged in,
         and call this link 'Login / Register' if they are not */}
-        {props.user.id ? <LogOutButton className="nav-link" /> : 'Login / Register'}
+        {props.user.id ? 'My Profile' : 'Login / Register'}
       </Link>
       {/* Always show this link since the about page is not protected */}
       {/* <Link className="nav-link" to="/about">
         About
       </Link> */}
-      {props.user.id && <Link className="nav-link" to="/home">
-        My Profile
-      </Link>}
       {props.user.access_id === (1 || 2) && <Link className="nav-link" to="/mentors">
         My Mentorships
       </Link>}
@@ -41,6 +40,7 @@ const Nav = (props) => (
           {/* <Link className="nav-link" to="/info">
             Info Page
           </Link> */}
+          <LogOutButton className="nav-link" />
         </>
       )}
     </div>
