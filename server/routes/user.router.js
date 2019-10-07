f3f0929ef6094b79e425d5d556193a3a8848f6ea
dmheisel <d.m.heisel@gmail.com>
2019-10-07 09:50:34 -0500

    fixed table name on user router and set to console log out error on registration

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -15,14 +15,14 @@ router.get('/', rejectUnauthenticated, (req, res) => {
 // Handles POST request with new user data
 // The only thing different from this and every other post we've seen
 // is that the password gets encrypted before being inserted
-router.post('/register', (req, res, next) => {  
+router.post('/register', (req, res, next) => {
   const username = req.body.username;
   const password = encryptLib.encryptPassword(req.body.password);
 
-  const queryText = 'INSERT INTO "user" (username, password) VALUES ($1, $2) RETURNING id';
+  const queryText = 'INSERT INTO "users" (username, password) VALUES ($1, $2) RETURNING id';
   pool.query(queryText, [username, password])
     .then(() => res.sendStatus(201))
-    .catch(() => res.sendStatus(500));
+    .catch((error) => { console.log('error on registering new user: ', error); res.sendStatus(500)});
 });
 
 // Handles login form authenticate/login POST
