222d3a21b55729241283b0c53a29bfbfa69635dd
dmheisel <d.m.heisel@gmail.com>
2019-10-08 13:59:04 -0500

    added  to router  & review page to use two column

--- src/components/MentorReview/MentorReview.jsx
+++ src/components/MentorReview/MentorReview.jsx
@@ -1,28 +1,36 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {
-	List,
-	ListItem,
-	ListItemText,
-	Paper,
-} from '@material-ui/core';
+import { List, ListItem, ListItemText, Paper } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import UserListItem from '../UserListItem/UserListItem';
+import TwoColumnLayout from '../TwoColumnLayout/TwoColumnLayout';
 
-import UserListItem from '../UserListItem/UserListItem'
+const styles = theme => ({
+	root: {
+		display: 'flexbox',
+		flexFlow: 'column-wrap',
+		width: '100%'
+		// margin: 'auto',
+		// marginTop: '5%'
+	}
+});
 
 class MentorReview extends Component {
 	componentDidMount = () => {
 		this.props.dispatch({ type: 'FETCH_PENDING_MENTORS' });
 	};
 	render() {
+		const { classes } = this.props;
 		const mentorsList =
 			this.props.pendingMentors &&
-			this.props.pendingMentors.map((mentor) => (
+			this.props.pendingMentors.map(mentor => (
 				<UserListItem key={mentor.id} user={mentor} />
 			));
 		return (
-			<Paper>
+			<TwoColumnLayout leftHeader='Pending Mentors' rightHeader=''>
 				<List>{mentorsList}</List>
-			</Paper>
+				<div></div>
+			</TwoColumnLayout>
 		);
 	}
 }
@@ -30,4 +38,4 @@ class MentorReview extends Component {
 const mapStateToProps = reduxStore => ({
 	pendingMentors: reduxStore.pendingMentorsReducer
 });
-export default connect(mapStateToProps)(MentorReview);
+export default connect(mapStateToProps)(withStyles(styles)(MentorReview));
