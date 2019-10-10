04e7a2ca7b37f1a8d7508675851120746c667fe6
Allyson Holdahl <allyson.holdahl@gmail.com>
2019-10-10 09:39:17 -0500

    protected job post, job search, job application components

--- src/components/JobApplication/JobApplication.jsx
+++ src/components/JobApplication/JobApplication.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {Typography, TextField, Button} from '@material-ui/core';
+import { Typography, TextField, Button } from '@material-ui/core';
 
 class JobApplication extends Component {
     state = {
@@ -9,10 +9,10 @@ class JobApplication extends Component {
         mentor_id: null,
         payment_terms: 'negotiable'
     }
-    componentDidMount(){
+    componentDidMount() {
         this.props.dispatch({
             type: 'FETCH_JOB_DETAIL',
-            payload: {id: Number(this.props.match.params.id)}
+            payload: { id: Number(this.props.match.params.id) }
         })
         this.props.dispatch({
             type: 'FETCH_ACTIVE_MENTORS'
@@ -32,20 +32,26 @@ class JobApplication extends Component {
             type: 'SUBMIT_APPLICATION',
             payload: {
                 ...this.state,
-                job_id: Number(this.props.match.params.id)}
+                job_id: Number(this.props.match.params.id)
+            }
         })
     }
-    render(){
+    render() {
+        let isAuthorized = () => {
+            return (this.props.user.access_id === 1)
+        }
         return (
             <div>
-            <h1>Job Application Page</h1>
-            {/* Client Info */}
-            {/* <div>
+                {isAuthorized() ?
+                    <div>
+                        <h1>Job Application Page</h1>
+                        {/* Client Info */}
+                        {/* <div>
                 <Typography>{this.props.job.username}</Typography>
                 <Typography>{this.props.job.location}</Typography>
             </div> */}
-            {/* Job Info */}
-            {/* <div>
+                        {/* Job Info */}
+                        {/* <div>
                 <Typography>{this.props.job.position_title}</Typography>
                 <Typography>{this.props.job.project_title}</Typography>
                 <Typography>Budget: $<span>{this.props.job.budget}</span></Typography>
@@ -58,45 +64,51 @@ class JobApplication extends Component {
                         );
                     })}
             </div> */}
-            {/* Cover Letter and Resume */}
-            <Typography variant="h6">Cover Letter</Typography>
-                <TextField id="standard-name"
-                    label="Add your cover letter..."
-                    value={this.state.cover_letter}
-                    onChange={(event) => { this.handleInput(event, 'cover_letter') }} 
-                    margin="normal"
-                    fullWidth={true}
-                    multiline={true}
-                    rows={4}
-                    />
-                    {/* Insert Resume attachment functionality here */}
-            {/* Mentor Info */}
-            <Typography variant="h6">Invite a Mentor</Typography>
-            {this.props.mentors.map((user)=>{
-                return (
-                    <div value={user.id} onChange={(event) => { this.handleInput(event, 'mentor_id') }}>
-                    <Typography>{user.username}</Typography>
-                    <Typography>{user.focus_skill}</Typography>
-                    <Typography><span>{user.skill_names.length}</span> Matching Skill{user.skill_names.length>1 && 's'}</Typography>
+                        {/* Cover Letter and Resume */}
+                        <div>
+                            <Typography variant="h6">Cover Letter</Typography>
+                            <TextField id="standard-name"
+                                label="Add your cover letter..."
+                                value={this.state.cover_letter}
+                                onChange={(event) => { this.handleInput(event, 'cover_letter') }}
+                                margin="normal"
+                                fullWidth={true}
+                                multiline={true}
+                                rows={4}
+                            />
+                        </div>
+                        {/* Insert Resume attachment functionality here */}
+                        {/* Mentor Info */}
+                        <div>
+                            <Typography variant="h6">Invite a Mentor</Typography>
+                            {this.props.mentors.map((user) => {
+                                return (
+                                    <div value={user.id} onChange={(event) => { this.handleInput(event, 'mentor_id') }}>
+                                        <Typography>{user.username}</Typography>
+                                        <Typography>{user.focus_skill}</Typography>
+                                        <Typography><span>{user.skill_names.length}</span> Matching Skill{user.skill_names.length > 1 && 's'}</Typography>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                        <br />
+                        {/* User Info */}
+                        <div>
+                            <Typography variant="h6">Your Account Information</Typography>
+                            <Typography>{this.props.user.username}</Typography>
+                            <Typography>{this.props.user.focus_skill}</Typography>
+                            <Typography>{this.props.user.location}</Typography>
+                            <Typography>{this.props.user.github_url}</Typography>
+                            <Typography>{this.props.user.linkedin_url}</Typography>
+                            <Typography>{this.props.user.website}</Typography>
+                        </div>
+                        {/* <Button variant="contained" color="secondary" onClick={this.props.history.push('/search/jobs')}>Cancel</Button> */}
+                        <Button variant="contained" color="primary" onClick={this.handleSubmit}>Submit</Button>
                     </div>
-                )
-            })}
-            <br/>
-            {/* User Info */}
-            <div>
-                <Typography variant="h6">Your Account Information</Typography>
-                <Typography>{this.props.user.username}</Typography>
-                <Typography>{this.props.user.focus_skill}</Typography>
-                <Typography>{this.props.user.location}</Typography>
-                <Typography>{this.props.user.github_url}</Typography>
-                <Typography>{this.props.user.linkedin_url}</Typography>
-                <Typography>{this.props.user.website}</Typography>
-            </div>
-            {/* <Button variant="contained" color="secondary" onClick={this.props.history.push('/search/jobs')}>Cancel</Button> */}
-            <Button variant="contained" color="primary" onClick={this.handleSubmit}>Submit</Button>
+                    : <Typography>You are not authorized to view this page.</Typography>}
             </div>
         )
-    }
+}
 }
 
 const mapStateToProps = (store) => {
