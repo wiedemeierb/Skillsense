f9cb4a5fcaabe95683eba8f9e36ce83e8ea4953e
dmheisel <d.m.heisel@gmail.com>
2019-10-10 10:31:23 -0500

    moved user skills onto the user reducer to consolidate data

--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -1,12 +1,14 @@
 const userReducer = (state = {}, action) => {
-  switch (action.type) {
-    case 'SET_USER':
-      return action.payload;
-    case 'UNSET_USER':
-      return {};
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case 'SET_USER':
+			return action.payload;
+		case 'SET_USER_SKILLS':
+			return { ...state, skills: action.payload };
+		case 'UNSET_USER':
+			return {};
+		default:
+			return state;
+	}
 };
 
 // user will be on the redux state at:
