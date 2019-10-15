d936dfa3118419862b2c04b58b81df1ab42d02a7
dmheisel <d.m.heisel@gmail.com>
2019-10-15 15:37:56 -0500

    message router base set up

--- server/routes/message.router.js
+++ server/routes/message.router.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
-const {
-    rejectUnauthenticated,
-} = require('../modules/authentication-middleware');
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 
 //GET ROUTE FOR ALL MESSAGES
 //NEEDS DEVELOPMENT: SHOULD PULL WHERE SENDER OR RECEIVER ID IS THE CURRENT USER, GROUP BY OTHER USER
 router.get('/', rejectUnauthenticated, (req, res) => {
-    const queryText = `SELECT * FROM "messages" ORDER BY "id" ASC;`
-    pool.query(queryText)
-        .then((result) => {
-            res.send(result.rows)
-        }).catch((error) => {
-            console.log(error);
-            res.sendStatus(500);
-        })
-})
+	const queryText = `
+select messages.id, message, date_time, sId, sName, rId, rName from messages
+join (select users.id as sId, users.username as sName from users) AS sent
+	ON sent.sId = messages.sender_id
+join (select users.id as rId, users.username as rName from users) as recipient
+	on recipient.rid = messages.recipient_id
+where messages.sender_id = $1 OR messages.recipient_id = $1;`;
+	pool.query(queryText, [req.user.id])
+		.then(result => {
+			res.send(result.rows);
+		})
+		.catch(error => {
+			console.log(error);
+			res.sendStatus(500);
+		});
+});
 
 module.exports = router;
