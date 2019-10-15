33e671fdd60475216333a9231cb2dc650ff8bfd2
Laney McKee <laneymckeephotography@gmail.com>
2019-10-15 02:42:14 -0500

    stylized navbar, added hover effects to links

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -54,8 +54,8 @@ class App extends Component {
             <MuiThemeProvider theme={theme}>
                 <CssBaseline />
                 <Router>
-                    <div>
-                        <Nav />
+                    <Nav />
+                    <div className="main">
                         <Switch>
                             {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
                             <Redirect exact from="/" to="/home" />
@@ -99,8 +99,8 @@ class App extends Component {
                             {/* If none of the other routes matched, we will show a 404. */}
                             <Route render={() => <h1>404</h1>} />
                         </Switch>
-                        <Footer />
                     </div>
+                    <Footer />
                 </Router>
             </MuiThemeProvider>
         );
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,83 +1,118 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import LogOutButton from '../LogOutButton/LogOutButton';
-import './Nav.css';
+//LOGO IMPORT
 import { ReactComponent as SkillSenseLogo } from '../../skillSenseLogo.svg';
+//STYLING IMPORTS
+import './Nav.css';
 
-const isAdmin = (props) => {
-  return props.user.access_id === 4
-}
+//for conditional rendering based on user type
+const isAdmin = props => {
+    return props.user.access_id === 4;
+};
+const isClient = props => {
+    return props.user.access_id === 3;
+};
+const isMentor = props => {
+    return props.user.access_id === 2;
+};
+const isStudent = props => {
+    return props.user.access_id === 1;
+};
 
-const isClient = (props) => {
-  return props.user.access_id === 3
+const Nav = props => (
+    <nav className="nav">
+        <Link to="/home">
+            <SkillSenseLogo alt="skill-sense logo" className="logo" />
+        </Link>
+        <div className="nav-right nav-hover">
+            {/* Always show this link since the about page is not protected */}
 
-}
+            {/* Show My Mentorships if user is Student or Mentor */}
+            {isStudent(props) || isMentor(props) ? (
+                <li>
+                    <Link data-hover="My MentorShips" className="nav-link" to="/mentors">
+                        My Mentorships
+                    </Link>
+                </li>
+            ) : null}
+            {/* Show Mentor Search if user is Student */}
+            {isStudent(props) && (
+                <li>
+                    <Link className="nav-link" to="/search/mentors">
+                        Mentor Search
+                    </Link>
+                </li>
+            )}
+            {/* Show Admin if user is Admin */}
+            {isAdmin(props) && (
+                <li>
+                    <Link className="nav-link" to="/admin">
+                        Admin
+                    </Link>
+                </li>
+            )}
+            {/* Show My Jobs if user is Student or Client */}
+            {isStudent(props) || isClient(props) ? (
+                <li>
+                    <Link className="nav-link" to="/jobs">
+                        My Jobs
+                    </Link>
+                </li>
+            ) : null}
 
-const isMentor = (props) => {
-  return props.user.access_id === 2
-}
+            {/* Show Job Search if user is Student */}
+            {isStudent(props) && (
+                <li>
+                    <Link className="nav-link" to="/search/jobs">
+                        Job Search
+                    </Link>
+                </li>
+            )}
+            {isClient(props) && (
+                <li>
+                    <Link className="nav-link" to="/jobs/new">
+                        Post New Job
+                    </Link>
+                </li>
+            )}
 
-const isStudent = (props) => {
-  return props.user.access_id === 1
-}
+            {/* Show this link if they are logged in or not,
+              but call this link 'Home' if they are logged in,
+              and call this link 'Login / Register' if they are not */}
+            <li>
+                <Link className="nav-link" to="/home">
+                    {/* Show My Profile if user is logged in, otherwise show Login/Register */}
+                    {props.user.id ? 'Profile' : 'Login / Register'}
+                </Link>
+            </li>
 
-const Nav = (props) => (
-  <div className="nav">
-    <Link to="/home">
-      <SkillSenseLogo alt="skill-sense logo" className="logo" />
-    </Link>
-    <div className="nav-right">
-      <Link className="nav-link" to="/home">
-        {/* Show this link if they are logged in or not,
-        but call this link 'Home' if they are logged in,
-        and call this link 'Login / Register' if they are not */}
-        {/* Show My Profile if user is logged in, otherwise show Login/Register */}
-        {props.user.id ? 'My Profile' : 'Login / Register'}
-      </Link>
-      {/* Always show this link since the about page is not protected */}
-
-      {/* Show My Mentorships if user is student or mentor */}
-      {isStudent(props) || isMentor(props) ? <Link className="nav-link" to="/mentors">
-        My Mentorships
-      </Link> : null}
-      {/* Show Mentor Search if user is student */}
-      {isStudent(props) && <Link className="nav-link" to="/search/mentors">
-        Mentor Search
-          </Link>}
-      {/* Show Admin if user is admin */}
-      {isAdmin(props) && <Link className="nav-link" to="/admin">
-        Admin
-      </Link>}
-      {/* Show My Jobs if user is student or client */}
-      {isStudent(props) || isClient(props) ? <Link className="nav-link" to="/jobs">
-        My Jobs
-          </Link> : null}
-      {/* Show Job Search if user is student */}
-      {isStudent(props) && <Link className="nav-link" to="/search/jobs">
-        Job Search
-      </Link>}
-      {isClient(props) && <Link className="nav-link" to="/jobs/new">
-        Post New Job
-      </Link>}
-      {/* Show the link to the info page and the logout button if the user is logged in */}
-      {props.user.id && (
-        <>
-          <Link className="nav-link" to="/messages">Messages</Link>
-          <LogOutButton className="nav-link" />
-        </>
-      )}
-    </div>
-  </div>
+            {/* Show the link to the info page and the logout button if the user is logged in */}
+            {props.user.id && (
+                <>
+                    <li>
+                        <Link className="nav-link" to="/messages">
+                            Messages
+                        </Link>
+                    </li>
+                    <li>
+                        <a className="nav-link" onClick={() => props.dispatch({ type: 'LOGOUT' })}>
+                            Log Out
+                        </a>
+                    </li>
+                </>
+            )}
+        </div>
+    </nav>
 );
 
 // Instead of taking everything from state, we just want the user
 // object to determine if they are logged in
-// if they are logged in, we show them a few more links 
+// if they are logged in, we show them a few more links
 // if you wanted you could write this code like this:
 // const mapStateToProps = ({ user }) => ({ user });
 const mapStateToProps = state => ({
-  user: state.user,
+    user: state.user
 });
 
 export default connect(mapStateToProps)(Nav);
