25046f6ae8be4fdee2838600ae98b2dea74d915d
dmheisel <d.m.heisel@gmail.com>
2019-10-15 15:38:10 -0500

    imports and base layout set up

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,36 +1,56 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {Typography} from '@material-ui/core';
+//material-ui imports
+import {
+	Typography,
+	Paper,
+	Button,
+	Grid,
+	List,
+	ListItem,
+	ListItemText,
+	IconButton
+} from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+
+//component import
+import TwoColumnLayout from '../TwoColumnLayout/TwoColumnLayout';
+
+const styles = theme => ({
+	root: {
+		display: 'flex'
+	},
+	messageList: {},
+	messageListItem: {}
+});
 
 class Messages extends Component {
-    componentDidMount() {
-        // gets all accepted mentorship relationships from the server and stores them in the allMentorsReducer
-        this.props.dispatch({
-            type: 'FETCH_ALL_MESSAGES'
-        });
-    }
+	componentDidMount() {
+		// gets all accepted mentorship relationships from the server and stores them in the allMentorsReducer
+		this.props.dispatch({
+			type: 'FETCH_ALL_MESSAGES'
+		});
+	}
 
-    render() {
-        return(
-            <div>
-                {this.props.messages.map((message) => {
-                    return(
-                        <Typography>
-                            {message.message}
-                        </Typography>
-                    )})}
-            </div>
-        );
-    }
-};
+	render() {
+		return (
+			<TwoColumnLayout leftHeader='Inbox' rightHeader='details'>
+				<Grid container justify='center' alignItems='center' spacing={2}>
+					<Grid item xs={12}></Grid>
+				</Grid>
+				<Grid container justify='center' alignItem='center' spacing={2}></Grid>
+			</TwoColumnLayout>
+		);
+	}
+}
 
 const mapStateToProps = store => {
-    return {
-        // mentors: store.allMentorsReducer,
-        // selectedUser: store.selectedUserReducer,
-        // user: store.user,
-        messages: store.allMessagesReducer
-    };
+	return {
+		// mentors: store.allMentorsReducer,
+		// selectedUser: store.selectedUserReducer,
+		// user: store.user,
+		messages: store.allMessagesReducer
+	};
 };
 
 export default connect(mapStateToProps)(Messages);
