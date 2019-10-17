575c8d3467e305268d80fd153cca0cfddca0c1ca
Allyson Holdahl <allyson.holdahl@gmail.com>
2019-10-17 08:30:24 -0500

    fixed issue with merge conflict

--- src/components/JobApplication/JobApplication.jsx
+++ src/components/JobApplication/JobApplication.jsx
@@ -132,9 +132,6 @@ class JobApplication extends Component {
                                 <Typography variant="h5" >
                                     {this.props.job.project_title}
                                 </Typography>
-                                {/* <Typography variant="body1" color="secondary">
-                                    {this.props.job.username}, {this.props.job.location}
-                                </Typography> */}
                             </Grid>
                             <Grid item xs={12}>
                                 <Typography variant="subtitle2" color="secondary">
@@ -210,9 +207,9 @@ class JobApplication extends Component {
                                     onChange={this.handleUploadInputChange}
                                 />
                                 <label htmlFor="file-upload">
-                                    <Button color="primary" component="span" size="large">
+                                    <Button variant="contained" color="primary" component="span">
                                         <AttachFileIcon />{' '}
-                                        <Typography variant="h6">Upload PDF File</Typography>
+                                        <Typography>Upload PDF File</Typography>
                                     </Button>
                                 </label>
                             </Grid>
@@ -312,215 +309,215 @@ class JobApplication extends Component {
     }
 //END BRANCH
     //START MASTER
-<!-- 	state = {
-		cover_letter: '',
-		mentor_id: null,
-		payment_terms: 'negotiable',
-		attachment_url: '',
-		file: null
-	};
-	componentDidMount() {
-		this.props.dispatch({
-			type: 'FETCH_JOB_DETAIL',
-			payload: { id: Number(this.props.match.params.id) }
-		});
-		this.props.dispatch({
-			type: 'FETCH_ACTIVE_MENTORS'
-		});
-	}
+// <!-- 	state = {
+// 		cover_letter: '',
+// 		mentor_id: null,
+// 		payment_terms: 'negotiable',
+// 		attachment_url: '',
+// 		file: null
+// 	};
+// 	componentDidMount() {
+// 		this.props.dispatch({
+// 			type: 'FETCH_JOB_DETAIL',
+// 			payload: { id: Number(this.props.match.params.id) }
+// 		});
+// 		this.props.dispatch({
+// 			type: 'FETCH_ACTIVE_MENTORS'
+// 		});
+// 	}
 
-	handleInput = (event, property) => {
-		this.setState({
-			...this.state,
-			[property]: event.target.value
-		});
-	};
+// 	handleInput = (event, property) => {
+// 		this.setState({
+// 			...this.state,
+// 			[property]: event.target.value
+// 		});
+// 	};
 
-	handleSubmit = event => {
-		event.preventDefault();
+// 	handleSubmit = event => {
+// 		event.preventDefault();
 
-		Swal.fire({
-			title: 'Are you sure?',
-			text: "You won't be able to redact your application!",
-			type: 'warning',
-			showCancelButton: true,
-			confirmButtonColor: '#04b8f3',
-			cancelButtonColor: '#505d68',
-			confirmButtonText: 'Yes, submit it!'
-		}).then(result => {
-			if (result.value) {
-				this.props.dispatch({
-					type: 'SUBMIT_APPLICATION',
-					payload: {
-						...this.state,
-						job_id: Number(this.props.match.params.id)
-					}
-				});this.props.history.push(`/jobs`)
-			}
-		});
-	};
+// 		Swal.fire({
+// 			title: 'Are you sure?',
+// 			text: "You won't be able to redact your application!",
+// 			type: 'warning',
+// 			showCancelButton: true,
+// 			confirmButtonColor: '#04b8f3',
+// 			cancelButtonColor: '#505d68',
+// 			confirmButtonText: 'Yes, submit it!'
+// 		}).then(result => {
+// 			if (result.value) {
+// 				this.props.dispatch({
+// 					type: 'SUBMIT_APPLICATION',
+// 					payload: {
+// 						...this.state,
+// 						job_id: Number(this.props.match.params.id)
+// 					}
+// 				});this.props.history.push(`/jobs`)
+// 			}
+// 		});
+// 	};
 
-	routeBack = () => {
-		this.props.history.push(`/search/jobs`);
-	};
+// 	routeBack = () => {
+// 		this.props.history.push(`/search/jobs`);
+// 	};
 
-	handleUploadInputChange = e => {
-		// console.log(e.target.files[0])
-		this.setState({ file: e.target.files[0] });
-	};
+// 	handleUploadInputChange = e => {
+// 		// console.log(e.target.files[0])
+// 		this.setState({ file: e.target.files[0] });
+// 	};
 
-	handleSelect = id => {
-		this.state.mentor_id === id
-			? this.setState({ mentor_id: null })
-			: this.setState({ mentor_id: id });
-	};
+// 	handleSelect = id => {
+// 		this.state.mentor_id === id
+// 			? this.setState({ mentor_id: null })
+// 			: this.setState({ mentor_id: id });
+// 	};
 
-	sortMentors = (mentors, jobSkills) => {
-		mentors.forEach(mentor => {
-			mentor.matchingSkillCount = mentor.skills.filter(tag =>
-				jobSkills.map(skill => skill.id).includes(tag.id)
-			).length;
-		});
-		return mentors.sort((a, b) => b.matchingSkillCount - a.matchingSkillCount);
-	};
+// 	sortMentors = (mentors, jobSkills) => {
+// 		mentors.forEach(mentor => {
+// 			mentor.matchingSkillCount = mentor.skills.filter(tag =>
+// 				jobSkills.map(skill => skill.id).includes(tag.id)
+// 			).length;
+// 		});
+// 		return mentors.sort((a, b) => b.matchingSkillCount - a.matchingSkillCount);
+// 	};
 
-	render() {
-		const { classes } = this.props;
-		let isStudent = () => {
-			return this.props.user.user_type === 'Student';
-		};
+// 	render() {
+// 		const { classes } = this.props;
+// 		let isStudent = () => {
+// 			return this.props.user.user_type === 'Student';
+// 		};
 
-		return (
-			<Grid
-				container
-				spacing={8}
-				justify='space-around'
-				alignItems='center'
-				className={classes.root}>
-				{isStudent() ? (
-					<Grid item container justify='space-around' spacing={4} xs={12}>
-						<Grid item xs={12}>
-							<Typography variant='h3' align='center'>
-								Job Application
-							</Typography>
-						</Grid>
-						{/* Job Info */}
-						<Grid item xs={4}>
-							<Typography color='primary' variant='h4'>
-								{this.props.job.position_title}
-							</Typography>
-							<Typography color='secondary' variant='h6'>
-								{this.props.job.project_title}
-							</Typography>
-						</Grid>
-						{/* Client Info */}
-						<Grid item xs={4}>
-							<Typography variant='h6'>{this.props.job.username}</Typography>
-							<Typography>{this.props.job.location}</Typography>
-						</Grid>
-						<Grid item xs={4}>
-							{this.props.job.skills && (
-								<SkillList skillList={this.props.job.skills} />
-							)}
-							<Typography>
-								Budget: $<span>{this.props.job.budget}</span>
-							</Typography>
-							<Typography>
-								Duration: <span>{this.props.job.duration}</span>
-							</Typography>
-						</Grid>
-						{/* Cover Letter and Resume */}
-						<Grid item container spacing={4} xs={12} justify='center'>
-							<Grid item xs={12}>
-								<Typography variant='h6' align='left'>
-									Cover Letter
-								</Typography>
-								<TextField
-									id='standard-name'
-									label='Add Your Cover Letter...'
-									value={this.state.cover_letter}
-									onChange={event => {
-										this.handleInput(event, 'cover_letter');
-									}}
-									margin='normal'
-									fullWidth={true}
-									multiline={true}
-								/>
-							</Grid>
-							{/* Insert Resume attachment functionality here */}
-							<Grid item xs={12}>
-								<TextField
-									helperText='Attach Any Additional Files As Needed'
-									type='file'
-									onChange={this.handleUploadInputChange}
-								/>
-								{/* Mentor Info */}
-							</Grid>
-						</Grid>
-						<Typography variant='h6'>Select One Of Your Mentors</Typography>
-						<Grid
-							className={classes.mentorList}
-							item
-							container
-							direction='column'
-							alignContent='flex-start'
-							justify='flex-start'>
-							{this.props.mentors &&
-								this.props.job &&
-								this.props.job.skills &&
-								this.sortMentors(this.props.mentors, this.props.job.skills).map(
-									listUser => {
-										return (
-											<JobApplicationMentorListItem
-												key={listUser.id}
-												selected={listUser.id === this.state.mentor_id}
-												listUser={listUser}
-												selectMentor={this.handleSelect}
-											/>
-										);
-									}
-								)}
-						</Grid>
-						<br />
-						{/* User Info */}
-						<Grid item xs={12}>
-							<Typography variant='h6'>Your Account Information:</Typography>
-							<Typography>{this.props.user.username}</Typography>
-							<Typography>{this.props.user.focus_skill}</Typography>
-							<Typography>{this.props.user.location}</Typography>
-							<Typography className={classes.link}>
-								<Link href={this.props.user.github_url}>GitHub Profile</Link>
-							</Typography>
-							<Typography className={classes.link}>
-								<Link href={this.props.user.linkedin_url}>LinkedIn Profile</Link>
-							</Typography>
-							{this.props.user.website_url && (
-								<Typography className={classes.link}>
-									<Link href={this.props.user.website_url}>
-										Portfolio/Personal Website
-									</Link>
-								</Typography>
-							)}
-						</Grid>
-						{/* <Button variant="contained" color="secondary" onClick={this.props.history.push('/search/jobs')}>Cancel</Button> */}
-						<Grid item xs={12}>
-							<div>
-							<Button variant='contained' color='primary' onClick={this.handleSubmit}>
-								Submit
-							</Button>
-								<Button
-									variant="contained" color="secondary" align="space-around" onClick={() => this.routeBack()}>
-									Back
-                    		</Button>
-							</div>
-						</Grid>
-					</Grid>
-				) : (
-					<Typography>You are not authorized to view this page.</Typography>
-				)}
-			</Grid>
-		);
-	} -->
+// 		return (
+// 			<Grid
+// 				container
+// 				spacing={8}
+// 				justify='space-around'
+// 				alignItems='center'
+// 				className={classes.root}>
+// 				{isStudent() ? (
+// 					<Grid item container justify='space-around' spacing={4} xs={12}>
+// 						<Grid item xs={12}>
+// 							<Typography variant='h3' align='center'>
+// 								Job Application
+// 							</Typography>
+// 						</Grid>
+// 						{/* Job Info */}
+// 						<Grid item xs={4}>
+// 							<Typography color='primary' variant='h4'>
+// 								{this.props.job.position_title}
+// 							</Typography>
+// 							<Typography color='secondary' variant='h6'>
+// 								{this.props.job.project_title}
+// 							</Typography>
+// 						</Grid>
+// 						{/* Client Info */}
+// 						<Grid item xs={4}>
+// 							<Typography variant='h6'>{this.props.job.username}</Typography>
+// 							<Typography>{this.props.job.location}</Typography>
+// 						</Grid>
+// 						<Grid item xs={4}>
+// 							{this.props.job.skills && (
+// 								<SkillList skillList={this.props.job.skills} />
+// 							)}
+// 							<Typography>
+// 								Budget: $<span>{this.props.job.budget}</span>
+// 							</Typography>
+// 							<Typography>
+// 								Duration: <span>{this.props.job.duration}</span>
+// 							</Typography>
+// 						</Grid>
+// 						{/* Cover Letter and Resume */}
+// 						<Grid item container spacing={4} xs={12} justify='center'>
+// 							<Grid item xs={12}>
+// 								<Typography variant='h6' align='left'>
+// 									Cover Letter
+// 								</Typography>
+// 								<TextField
+// 									id='standard-name'
+// 									label='Add Your Cover Letter...'
+// 									value={this.state.cover_letter}
+// 									onChange={event => {
+// 										this.handleInput(event, 'cover_letter');
+// 									}}
+// 									margin='normal'
+// 									fullWidth={true}
+// 									multiline={true}
+// 								/>
+// 							</Grid>
+// 							{/* Insert Resume attachment functionality here */}
+// 							<Grid item xs={12}>
+// 								<TextField
+// 									helperText='Attach Any Additional Files As Needed'
+// 									type='file'
+// 									onChange={this.handleUploadInputChange}
+// 								/>
+// 								{/* Mentor Info */}
+// 							</Grid>
+// 						</Grid>
+// 						<Typography variant='h6'>Select One Of Your Mentors</Typography>
+// 						<Grid
+// 							className={classes.mentorList}
+// 							item
+// 							container
+// 							direction='column'
+// 							alignContent='flex-start'
+// 							justify='flex-start'>
+// 							{this.props.mentors &&
+// 								this.props.job &&
+// 								this.props.job.skills &&
+// 								this.sortMentors(this.props.mentors, this.props.job.skills).map(
+// 									listUser => {
+// 										return (
+// 											<JobApplicationMentorListItem
+// 												key={listUser.id}
+// 												selected={listUser.id === this.state.mentor_id}
+// 												listUser={listUser}
+// 												selectMentor={this.handleSelect}
+// 											/>
+// 										);
+// 									}
+// 								)}
+// 						</Grid>
+// 						<br />
+// 						{/* User Info */}
+// 						<Grid item xs={12}>
+// 							<Typography variant='h6'>Your Account Information:</Typography>
+// 							<Typography>{this.props.user.username}</Typography>
+// 							<Typography>{this.props.user.focus_skill}</Typography>
+// 							<Typography>{this.props.user.location}</Typography>
+// 							<Typography className={classes.link}>
+// 								<Link href={this.props.user.github_url}>GitHub Profile</Link>
+// 							</Typography>
+// 							<Typography className={classes.link}>
+// 								<Link href={this.props.user.linkedin_url}>LinkedIn Profile</Link>
+// 							</Typography>
+// 							{this.props.user.website_url && (
+// 								<Typography className={classes.link}>
+// 									<Link href={this.props.user.website_url}>
+// 										Portfolio/Personal Website
+// 									</Link>
+// 								</Typography>
+// 							)}
+// 						</Grid>
+// 						{/* <Button variant="contained" color="secondary" onClick={this.props.history.push('/search/jobs')}>Cancel</Button> */}
+// 						<Grid item xs={12}>
+// 							<div>
+// 							<Button variant='contained' color='primary' onClick={this.handleSubmit}>
+// 								Submit
+// 							</Button>
+// 								<Button
+// 									variant="contained" color="secondary" align="space-around" onClick={() => this.routeBack()}>
+// 									Back
+//                     		</Button>
+// 							</div>
+// 						</Grid>
+// 					</Grid>
+// 				) : (
+// 					<Typography>You are not authorized to view this page.</Typography>
+// 				)}
+// 			</Grid>
+// 		);
+// 	} -->
 //END master
 }
 
