bba262d7835cd4986c1e5b41dabd8213af994269
Allyson Holdahl <allyson.holdahl@gmail.com>
2019-10-23 09:40:23 -0500

    code cleanup on router files

--- server/routes/mentor.router.js
+++ server/routes/mentor.router.js
@@ -310,8 +310,7 @@ router.get('/pending', rejectIfNotAdmin, (req, res) => {
 		mentor_status.mentor_status
 		ORDER BY "id" DESC;`;
 
-	pool
-		.query(queryText)
+	pool.query(queryText)
 		.then(result => {
 			//creates an array in each row that consists of skill objects {id: name}
 			result.rows.forEach(row => {
@@ -331,22 +330,22 @@ router.get('/pending', rejectIfNotAdmin, (req, res) => {
 router.put('/accept/:id', rejectUnauthenticated, (req, res) => {
 	const queryText = `UPDATE "student_mentor" SET "accepted" = true WHERE "mentor_id" = $1 AND "student_id" = $2;`
 	pool.query(queryText, [req.user.id, req.params.id])
-	.then(result => {
-		res.sendStatus(200)
-	}).catch(error => {
-		res.sendStatus(500)
-	})
+		.then(result => {
+			res.sendStatus(200)
+		}).catch(error => {
+			res.sendStatus(500)
+		})
 })
 
 /** DELETE ROUTE FOR MENTOR TO DECLINE STUDENT CONNECTION **/
 router.delete('/decline/:id', rejectUnauthenticated, (req, res) => {
 	const queryText = `DELETE FROM "student_mentor" WHERE "mentor_id" = $1 AND "student_id" = $2;`
 	pool.query(queryText, [req.user.id, req.params.id])
-	.then(result => {
-		res.sendStatus(200)
-	}).catch(error => {
-		res.sendStatus(500)
-	})
+		.then(result => {
+			res.sendStatus(200)
+		}).catch(error => {
+			res.sendStatus(500)
+		})
 })
 
 /** POST (STUDENT: SEND MENTOR REQUEST) ROUTE **/
@@ -385,9 +384,7 @@ router.patch(`/admin/:id`, rejectIfNotAdmin, (req, res) => {
 	//expects a req.body with {newStatus: #}
 	const queryText = `UPDATE users SET approved_mentor = $1  WHERE users.id = $2`;
 	const values = [req.body.newStatus, req.params.id];
-
-	pool
-		.query(queryText, values)
+	pool.query(queryText, values)
 		.then(result => {
 			res.sendStatus(200);
 		})
@@ -402,8 +399,7 @@ router.patch(`/request`, rejectIfNotMentor, (req, res) => {
 	const queryText = `UPDATE users SET approved_mentor = 2 WHERE users.id = $1`;
 	const values = [req.user.id];
 
-	pool
-		.query(queryText, values)
+	pool.query(queryText, values)
 		.then(result => {
 			res.sendStatus(200);
 		})
